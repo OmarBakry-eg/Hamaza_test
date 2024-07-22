ee79988ba428823ac8b2898155db688c80bbb7da
OmarBakry-eg <omarbakry202020@gmail.com>
2024-07-22 17:00:52 +0300

    adjusting login one more time after verification

--- lib/feature/login/presentation/screens/email_verification_page.dart
+++ lib/feature/login/presentation/screens/email_verification_page.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_ui_auth/firebase_ui_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:news_app_test/core/util/logger.dart';
+import 'package:news_app_test/feature/login/presentation/screens/sign_in_page.dart';
 
 class EmailVerificationPage extends StatelessWidget {
   const EmailVerificationPage({super.key});
@@ -11,7 +12,14 @@ class EmailVerificationPage extends StatelessWidget {
       actions: [
         EmailVerifiedAction(() {
           Logger.logInfo("EmailVerifiedAction");
-          Navigator.pop(context);
+          FirebaseUIAuth.signOut(context: context);
+          Navigator.pushReplacement(
+              context,
+              MaterialPageRoute(
+                  fullscreenDialog: true,
+                  builder: (c) => const SignInPage(
+                        headerTitle: "Now please sign in to confirm your data",
+                      )));
         }),
         AuthCancelledAction((context) {
           Logger.logInfo("AuthCancelledAction");
